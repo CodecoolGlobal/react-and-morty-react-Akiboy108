30ee44915b15c3cd5d5e17d9754d74e3985632e3
Akos <barcza.akos15@gmail.com>
2022-09-12 14:08:13 +0200

    rename Locations to locations | L -> l

--- src/Components/Layout/layout.jsx
+++ src/Components/Layout/layout.jsx
@@ -7,40 +7,40 @@ import LayoutDescriptions from "./../LayoutDescriptions/LayoutDescriptions"
 import { useState } from "react";
 
 export default function Layout(props) {
-  const [isClickedCharacters, setIsClickedCharacters] = useState(false);
-  const [isClickedLocations, setIsClickedLocations] = useState(false);
-  return (
-    <div className="Layout">
-      <img src={logo} alt="" className="Layout__img"></img>
-      <div className="Layout__buttons">
-        <span
-          className="Layout__button"
-          onClick={() => {
-            setIsClickedLocations(false);
-            setIsClickedCharacters(true);
-          }}
-        >
-          Characters
-        </span>
-        <span
-          className="Layout__button"
-          onClick={() => {
-            setIsClickedLocations(true);
-            setIsClickedCharacters(false);
-          }}
-        >
-          Locations
-        </span>
-      </div>
-      <div className="Layout__content">
-        {(!isClickedCharacters && !isClickedLocations) && <LayoutDescriptions></LayoutDescriptions>}
-        {isClickedCharacters && (
-          <Characters characters={props.characters}></Characters>
-        )}
-        {isClickedLocations && (
-          <Locations locations={props.locations}></Locations>
-        )}
-      </div>
-    </div>
-  );
+    const [isClickedCharacters, setIsClickedCharacters] = useState(false);
+    const [isClickedLocations, setIsClickedLocations] = useState(false);
+    return (
+        <div className="Layout">
+            <img src={logo} alt="" className="Layout__img"></img>
+            <div className="Layout__buttons">
+                <span
+                    className="Layout__button"
+                    onClick={() => {
+                        setIsClickedLocations(false);
+                        setIsClickedCharacters(true);
+                    }}
+                >
+                    Characters
+                </span>
+                <span
+                    className="Layout__button"
+                    onClick={() => {
+                        setIsClickedLocations(true);
+                        setIsClickedCharacters(false);
+                    }}
+                >
+                    Locations
+                </span>
+            </div>
+            <div className="Layout__content">
+                {(!isClickedCharacters && !isClickedLocations) && <LayoutDescriptions></LayoutDescriptions>}
+                {isClickedCharacters && (
+                    <Characters characters={props.characters}></Characters>
+                )}
+                {isClickedLocations && (
+                    <Locations locations={props.locations}></Locations>
+                )}
+            </div>
+        </div>
+    );
 }
