7726432286d59c6a8e1cfdb15a061ba17644a005
Akos <barcza.akos15@gmail.com>
2022-09-12 14:06:20 +0200

    processing and using incoming data for locations

--- src/Components/Locations/locations.jsx
+++ src/Components/Locations/locations.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import App from './App';
-import Location from './Location';
+import Location from '../Location/Location';
 
 const Locations = (props) => {
-    const locations = props.locations;
-    console.log('LOC::::::S:::::::::', locations)
-
     return (
         <div id='Container'>
-            <Location location={locations} />
+            {props.locations.results.map(x => <Location location={x} />) }
         </div>
     )
 }
