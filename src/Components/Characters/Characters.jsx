07b07033a01db9473128966b93077162f1d3e135
tercsd <tercsd@gmail.com>
2022-09-13 20:45:30 +0200

    add infinite scroll to Characters component

--- src/Components/Characters/Characters.jsx
+++ src/Components/Characters/Characters.jsx
@@ -1,61 +1,91 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { useCharacters } from "../../api/useData";
 import "./Characters.css";
-import CharacterDisplay from "../CharacterDisplay/CharacterDisplay";
 import Pagination from "../Pagination/Pagination";
+import CharacterDisplay from "../CharacterDisplay/CharacterDisplay";
+import useScrollList from "../../Hooks/useScrollList";
 
 export default function Characters() {
   const [page, setPage] = useState(1);
-  const characters = useCharacters(page);
-  const [character, setCharacter] = useState("");
+  const charactersData = useCharacters(page);
+
+  const [characterSelect, setCharacterSelect] = useState("");
+
+  const [pageScroll, setPageScroll] = useState(page);
+  const { items, setItems, hasMore, loading, error } = useScrollList(pageScroll, "character");
+
+  const observer = useRef();
+  const lastItemElementRef = useCallback(
+    (node) => {
+      // console.log(node);
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          console.log("Last item visible");
+          setPageScroll((prevPageScroll) => prevPageScroll + 1);
+          console.log(pageScroll);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
   const newPage = (newPage = 1) => {
     setPage(newPage);
+    setItems([]);
+    setPageScroll(newPage);
   };
 
   const selectCharacter = (newCharacter) => {
-    console.log(newCharacter);
-    console.log(character);
-    if (newCharacter === character) {
-      setCharacter("");
+    if (newCharacter === characterSelect) {
+      setCharacterSelect("");
     } else {
-      setCharacter(newCharacter);
+      setCharacterSelect(newCharacter);
     }
   };
 
   return (
     <div id="characters">
       <h2>Characters</h2>
-      {characters === "Loading..." ? (
+      {charactersData === "Loading..." ? (
         0
       ) : (
-        <Pagination data={characters} page={page} newPage={newPage} />
+        <Pagination data={charactersData} page={page} newPage={newPage} />
       )}
       <div id="characterList">
-        {characters === "Loading..."
-          ? characters
-          : characters.results.map((char, index) => (
-              <>
-                <div className="characterRow">
+        {!items
+          ? "Loading..."
+          : items.map((char, index) => {
+              return (
+                <div key={char.name + index}>
                   <div
-                    className="characterLeft pointer"
-                    key={index}
-                    onClick={(e) => selectCharacter(e.target.innerText)}
+                    className="characterRow"
+                    ref={items.length === index + 1 ? lastItemElementRef : null}
                   >
-                    {char.name}
+                    <div
+                      className="characterLeft pointer"
+                      key={index}
+                      onClick={(e) => selectCharacter(e.target.innerText)}
+                    >
+                      {char.name}
+                    </div>
+                    <div className="characterRight">{char.species}</div>
                   </div>
-                  <div className="characterRight">{char.species}</div>
+                  {characterSelect === char.name ? (
+                    <div id="characterDisplay" className="characterRow">
+                      <CharacterDisplay character={char} />
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  <div>{loading && "Loading..."}</div>
+                  <div>{error && "Error"}</div>
                 </div>
-                {character === char.name ? (
-                  <div id="characterDisplay" className="characterRow">
-                    <CharacterDisplay character={char} />
-                  </div>
-                ) : (
-                  ""
-                )}
-              </>
-            ))}
+              );
+            })}
       </div>
     </div>
   );
