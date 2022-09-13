116b9fe6278bbfa2c311f6fd5b9fd89b1d011c72
Akos <barcza.akos15@gmail.com>
2022-09-13 20:29:12 +0200

    modify css, add classname to noname div, solve props problem

--- src/Components/Locations/locations.jsx
+++ src/Components/Locations/locations.jsx
@@ -7,54 +7,54 @@ import "./Locations.css";
 import Pagination from "../Pagination/Pagination";
 
 const Locations = (props) => {
-  const [page, setPage] = useState(1);
-  const [location, setLocation] = useState("");
-  const locations = useLocations(page);
+    const [page, setPage] = useState(1);
+    const [location, setLocation] = useState("");
+    const locations = useLocations(page);
 
-  function selectLocation(target) {
-    setLocation(target);
-    console.log(target);
-  }
+    function selectLocation(target) {
+        setLocation(target);
+        console.log(target);
+    }
 
-  const newPage = (newPage = 1) => {
-    setPage(newPage);
-  };
+    const newPage = (newPage = 1) => {
+        setPage(newPage);
+    };
 
-  return (
-    <div>
-      <h2>Locations</h2>
+    return (
+        <div>
+            <h2>Locations</h2>
 
-      {locations === "Loading..." ? (
-        0
-      ) : (
-        <Pagination data={locations} page={page} newPage={newPage} />
-      )}
+            {locations === "Loading..." ? (
+                0
+            ) : (
+                <Pagination data={locations} page={page} newPage={newPage} />
+            )}
 
-      <div id="Container">
-        {locations === "Loading..."
-          ? locations
-          : locations.results.map((loc, index) => (
-              <div id="location">
-                <div
-                  className="Click pointer"
-                  key={index}
-                  onClick={(e) => selectLocation(e.target.innerText)}
-                >
-                  <Location location={loc} />
-                </div>
-                {location === loc.name ? (
-                  <div>
-                    {" "}
-                    <Locationdisplay location={loc} />{" "}
-                  </div>
-                ) : (
-                  ""
-                )}
-              </div>
-            ))}
-      </div>
-    </div>
-  );
+            <div id="Container">
+                {locations === "Loading..."
+                    ? locations
+                    : locations.results.map((loc, index) => (
+                        <div id="location">
+                            <div
+                                className="Click pointer"
+                                key={index}
+                                onClick={(e) => selectLocation(e.target.innerText)}
+                            >
+                                <Location location={loc} />
+                            </div>
+                            {location === loc.name ? (
+                                <div className="locDispCont">
+                                    {" "}
+                                    <Locationdisplay location={loc} />{" "}
+                                </div>
+                            ) : (
+                                ""
+                            )}
+                        </div>
+                    ))}
+            </div>
+        </div>
+    );
 };
 
 export default Locations;
