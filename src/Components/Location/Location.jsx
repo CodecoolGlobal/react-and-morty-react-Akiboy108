a3b143de6d17cfcabd09f7912f8a3ba58f57c804
Akos <barcza.akos15@gmail.com>
2022-09-13 10:14:33 +0200

    set row height to fix 60px instead of using padding

--- src/Components/Location/Location.jsx
+++ src/Components/Location/Location.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Location.css';
+import Locationdisplay from '../LocationDisplay/LocationDisplay';
+import { useState } from 'react';
 
 const Location = (props) => {
+    const [location, setLocation] = useState('');
     const name = props.location.name;
     const type = props.location.type;
 
+    function showProp(target) {
+        setLocation(target)
+    }
+
     return (
-        <div id='Container'>
+        <div onClick={(e) => showProp(e.target.innerText)} id='Container'>
             <div className='row'>
                 <div id='locationName'>{name}</div>
                 <div id='locationType'>{type}</div>
