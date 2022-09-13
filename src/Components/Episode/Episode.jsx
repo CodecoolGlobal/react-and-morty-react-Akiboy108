dd105caad939f4734f5bc756c62c0f29d14efb98
tercsd <tercsd@gmail.com>
2022-09-13 11:45:34 +0200

    add episode tiles

--- src/Components/Episode/Episode.jsx
+++ src/Components/Episode/Episode.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Episode.css"
 
 export default function Episode({ episode, index }) {
   const ep = episode.split("/");
   return (
-    <span className="Episode" key={index}>
-      {`${ep[ep.length - 1]}, `}
+    <span className="characterEpisode" key={index}>
+      {`${ep[ep.length - 1]}`}
     </span>
   );
 }
