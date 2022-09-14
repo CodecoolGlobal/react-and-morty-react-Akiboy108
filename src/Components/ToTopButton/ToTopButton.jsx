1e7830e4b7eb99bdb1899cc175fed9bb2256914b
tercsd <tercsd@gmail.com>
2022-09-14 16:09:53 +0200

    add toTopButton cleanup

--- src/Components/ToTopButton/ToTopButton.jsx
+++ src/Components/ToTopButton/ToTopButton.jsx
@@ -1,37 +1,42 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import './ToTopButton.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./ToTopButton.css";
 import up from "./../../img/up.png";
 
-
 export default function ToTopButton() {
-    const [topButton, setTopButton] = useState(false);
+  const [topButton, setTopButton] = useState(false);
 
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            if (window.scrollY > 500) {
-                setTopButton(true);
-            }
-            else {
-                setTopButton(false);
-            }
-        });
-    }, []);
+  useEffect(() => {
+    const scrollEventListener = () => {
+      if (window.scrollY > 500) {
+        setTopButton(true);
+      } else {
+        setTopButton(false);
+      }
+    };
 
-    const scrollUp = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        })
-    }
+    window.addEventListener("scroll", scrollEventListener);
 
-    return (
-        <div>
-            {topButton && (
-                <button className='toTop fa-solid fa-angle-up' onClick={scrollUp}>
-                    <img src={up} alt="toTop" />
-                </button>
-            )}
-        </div>
-    )
+    return () => {
+      window.removeEventListener("scroll", scrollEventListener);
+    };
+    
+  }, []);
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div>
+      {topButton && (
+        <button className="toTop fa-solid fa-angle-up" onClick={scrollUp}>
+          <img src={up} alt="toTop" />
+        </button>
+      )}
+    </div>
+  );
 }
